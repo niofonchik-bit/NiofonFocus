65f757925fb434b5aa7994fe7a8eab4d206e9f2e
Niofon <niofonchik@gmail.com>
2026-06-14 12:54:28 +0300

    тест

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <main className='page'>
-            <h1>Niofocus</h1>
+            <h1>Тестирование подключения отдельного проекта как поддомена на хостинге</h1>
         </main>
     );
 }
